a2401e6eaf5caebf49dba115b0638f4408dedb95
lipsanen <jukspaz@gmail.com>
2022-07-15 19:39:04 +0300

    Add more version support

--- src/version_utils.c
+++ src/version_utils.c
@@ -2,27 +2,20 @@
 #include "utils.h"
 #include <string.h>
 
-struct version_pair
-{
-  const char* game_directory;
+struct version_pair {
+  const char *game_directory;
   demo_version version;
 };
 
 typedef struct version_pair version_pair;
 
 static version_pair versions[] = {
-  {"portal2", portal2},
-  {"csgo", csgo},
-  {"left4dead2", l4d2},
-  {"left4dead", l4d}
-};
+    {"aperturetag", portal2}, {"portal2", portal2}, {"portalreloaded", portal2}, {"TWTM", portal2},
+    {"csgo", csgo},           {"left4dead2", l4d2}, {"left4dead", l4d}};
 
-demo_version get_demo_version(demogobbler_header* header)
-{
-  for(size_t i=0; i < ARRAYSIZE(versions); ++i)
-  {
-    if(strcmp(header->game_directory, versions[i].game_directory) == 0)
-    {
+demo_version get_demo_version(demogobbler_header *header) {
+  for (size_t i = 0; i < ARRAYSIZE(versions); ++i) {
+    if (strcmp(header->game_directory, versions[i].game_directory) == 0) {
       return versions[i].version;
     }
   }
